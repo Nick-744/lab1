d90c584231d7392fa475fd01eaca932e9864f161
ΓΕΡΟΝΤΑΣ ΝΙΚΟΛΑΟΣ <up1092813>
2025-02-24 12:57:32 +0200

    Ypoloipa Erothmata

--- lab1_now.py
+++ lab1_now.py
@@ -16,8 +16,8 @@ def more(text):
 #    html = response.text
 #    more(html)
 
-# Ερώτημα 1ο
-url = input("Give url:\t")
+# Ερώτημα 1ο - Ζητάει από τον χρήστη ένα URL
+url = input("- Give url:\t")
 
 http_start = "https://"
 if not url.startswith("https://"):
@@ -25,6 +25,24 @@ if not url.startswith("https://"):
 
 print(url)
 
-# Ερώτημα 2ο
+# Ερώτημα 2ο - Πραγματοποιεί ένα αίτημα HTTP σε αυτό το URL
 with requests.get(url) as response:
-    print(response.text)
+    # Ερώτημα 3ο - Τυπώνει τις κεφαλίδες (headers) της απόκρισης HTTP
+    #print(f"\n- Κεφαλίδες της απόκρισης HTTP:")
+    #for key in response.headers:
+    #    print(f"Name: {key}, Value: {response.headers[key]}")
+    
+    # Ερώτημα 4ο - Τροποποιήστε τον κώδικα ώστε να απαντάει για το
+    #              URL που έδωσε ο χρήστης με τις εξής πληροφορίες:
+
+    # - Ποιο είναι το λογισμικό που χρησιμοποιεί ο εξυπηρετητής (ο web server)
+    # για να απαντήσει στο αίτημα;
+    print(f"\nServer: {response.headers.get('Server')}")
+
+    # - Αν η σελίδα χρησιμοποιεί cookies, και αν ναι
+    print(f"\nHas cookies: {'Set-Cookie' in response.headers}")
+
+    # - Το όνομα κάθε cookie και για πόσο διάστημα θα είναι έγκυρο.
+    print(" ") # Καλύτερη αισθητική
+    for cookie in response.cookies:
+        print(f"Name: {cookie.name}, Expires:")
