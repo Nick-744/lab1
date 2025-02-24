6efe3408fe7edc39151b615f616355a12f80f3b0
G. Nikolaos X. <nick744gr@gmail.com>
2025-02-24 21:09:08 +0200

    Done! Unix timestamp converted to successfully!

--- lab1_now.py
+++ lab1_now.py
@@ -1,4 +1,5 @@
 import requests # Εισαγωγή της βιβλιοθήκης
+from datetime import datetime
 
 def more(text):
     count = 0
@@ -13,38 +14,44 @@ def more(text):
     return;
 
 '''
-url = 'http://python.org/'  # προσδιορισμός του url
+- Example:
 
-with requests.get(url) as response:  # το αντικείμενο response
+url = 'http://python.org/' # προσδιορισμός του url
+with requests.get(url) as response: # το αντικείμενο response
     html = response.text
     more(html)
 '''
 
-# Ερώτημα 1ο - Ζητάει από τον χρήστη ένα URL
-url = input("- Give url:\t")
+# Ερώτημα 1ο - Ζητάει από τον χρήστη ένα URL:
+url = input("- Give url: ")
 http_start = "https://"
-if not url.startswith("https://"):
-    url = "https://" + url
+if not url.startswith(http_start):
+    url = http_start + url
 print(url)
 
-# Ερώτημα 2ο - Πραγματοποιεί ένα αίτημα HTTP σε αυτό το URL
+# Ερώτημα 2ο - Πραγματοποιεί ένα αίτημα HTTP σε αυτό το URL:
 with requests.get(url) as response:
-    # Ερώτημα 3ο - Τυπώνει τις κεφαλίδες (headers) της απόκρισης HTTP
-    '''print(f"\n- Κεφαλίδες της απόκρισης HTTP:")
+    # Ερώτημα 3ο - Τυπώνει τις κεφαλίδες (headers) της απόκρισης HTTP:
+    print(f"\n- Κεφαλίδες της απόκρισης HTTP:")
     for key in response.headers:
-        print(f"Name: {key}, Value: {response.headers[key]}")'''
+        print(f"Name: {key}, Value: {response.headers[key]}")
     
     # Ερώτημα 4ο - Τροποποιήστε τον κώδικα ώστε να απαντάει για το
     #              URL που έδωσε ο χρήστης με τις εξής πληροφορίες:
-
-    # - Ποιο είναι το λογισμικό που χρησιμοποιεί ο εξυπηρετητής (ο web server)
-    # για να απαντήσει στο αίτημα;
+    '''
+    α) Ποιο είναι το λογισμικό που χρησιμοποιεί ο εξυπηρετητής (ο web server)
+       για να απαντήσει στο αίτημα;
+    '''
     print(f"\nServer: {response.headers.get('Server')}")
 
-    # - Αν η σελίδα χρησιμοποιεί cookies, και αν ναι
+    '''
+    β) Αν η σελίδα χρησιμοποιεί cookies, και αν ναι:
+    '''
     print(f"\nHas cookies: {'Set-Cookie' in response.headers}")
 
-    # - Το όνομα κάθε cookie και για πόσο διάστημα θα είναι έγκυρο.
-    print(" ") # Καλύτερη αισθητική
+    '''
+    γ) Το όνομα κάθε cookie και για πόσο διάστημα θα είναι έγκυρο:
+    '''
+    print("\nMore info about cookies:")
     for cookie in response.cookies:
-        print(f"Name: {cookie.name}, Expires:")
+        print(f"Name: {cookie.name}, Expires: {datetime.fromtimestamp(cookie.expires)}")
